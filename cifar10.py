dcf272b625f1cebb2d32934d568512062dfdfbb1
Dat Nguyen <tpc2102@gmail.com>
2020-04-21 22:41:02 +0700

    add notebook

--- cifar10.py
+++ cifar10.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 from resnext import ResNeXtModel
 from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 import matplotlib.pyplot as plt
 
 print(tf.__version__)
@@ -13,10 +15,20 @@ x_train /= 255.
 x_test /= 255.
 input_shape = x_train.shape[1:]
 
-model = ResNeXtModel(input_shape=input_shape, nb_classes=10, structure=[3, 3, 3])
-model.fit(x_train, y_train, x_test, y_test, 32, 100)
+generator = ImageDataGenerator(rotation_range=15,
+                               width_shift_range=5./32,
+                               height_shift_range=5./32,
+                               horizontal_flip=True)
 
-model.test(x_test, y_test)
+generator.fit(x_train)
+batch_size = 100
+
+lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, patience=10, min_lr=1e-6)
+
+callbacks = [lr_reducer]
+model = ResNeXtModel(input_shape=input_shape, nb_classes=10, structure=[3, 3, 3], cardinality=8, width=4).get_model()
+model.fit(generator.flow(x_train, y_train, batch_size=batch_size), steps_per_epoch=len(x_train) // batch_size, epochs=300, callbacks=callbacks, validation_data=(x_test, y_test), validation_steps=x_test.shape[0] // batch_size, verbose=2)
+
 
 
 
--- resnext.py
+++ resnext.py
@@ -3,17 +3,19 @@ import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Input, GlobalAveragePooling2D, Dense, \
     Softmax, add, Lambda, concatenate
+from tensorflow_addons.optimizers import AdamW
 from tensorflow.keras.initializers import he_normal
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.activations import relu
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.metrics import sparse_categorical_accuracy
-from sklearn.metrics import accuracy_score
+from tensorflow.keras.backend import shape
+from tensorflow import Variable
 
 
 class ResNeXtModel:
-    def __init__(self, input_shape, nb_classes, structure, cardinality=8, width=4, l2_weight=5e-4):
+    def __init__(self, input_shape, nb_classes, structure, cardinality=16, width=4, l2_weight=5e-4):
         self.width = width
         self.cardinality = cardinality
         self.structure = structure
@@ -22,10 +24,11 @@ class ResNeXtModel:
         self.l2_weight = l2_weight
         self.model = self.build_resnext()
         self.model.summary()
-        self.model.compile(optimizer=Adam(lr=1e-3), loss=sparse_categorical_crossentropy, metrics=[sparse_categorical_accuracy])
+        step = Variable(0, trainable=False)
+        self.model.compile(optimizer=Adam(learning_rate=1e-3), loss=sparse_categorical_crossentropy, metrics=[sparse_categorical_accuracy])
 
     def initial_block(self, inputs):
-        x = Conv2D(64, (3, 3), padding='same', kernel_initializer=he_normal(), kernel_regularizer=l2(self.l2_weight))(
+        x = Conv2D(64, (3, 3), padding='same', kernel_initializer=he_normal(), kernel_regularizer=l2(self.l2_weight), use_bias=False)(
             inputs)
         x = BatchNormalization()(x)
         x = Activation(relu)(x)
@@ -37,21 +40,21 @@ class ResNeXtModel:
         x = Softmax()(x)
         return x
 
-    def bottleneck_block(self, inputs, nb_filters, cardinality, strides):
+    def bottleneck_block(self, inputs, nb_filters, strides, pass_filter=False):
         x_skip = inputs
-        if strides == 2:
+        if pass_filter:
             x_skip = Conv2D(nb_filters * 2, (1, 1), padding='same', kernel_initializer=he_normal(),
-                            kernel_regularizer=l2(self.l2_weight), strides=(strides, strides))(x_skip)
+                            kernel_regularizer=l2(self.l2_weight), strides=(strides, strides), use_bias=False)(x_skip)
             x_skip = BatchNormalization()(x_skip)
         x = Conv2D(nb_filters, (1, 1), padding='same', kernel_initializer=he_normal(),
-                   kernel_regularizer=l2(self.l2_weight))(inputs)
+                   kernel_regularizer=l2(self.l2_weight), use_bias=False)(inputs)
         x = BatchNormalization()(x)
         x = Activation(relu)(x)
 
-        x = self.grouped_block(x, int(nb_filters / cardinality), cardinality, strides)
+        x = self.grouped_block(x, int(nb_filters / self.cardinality), strides)
 
         x = Conv2D(nb_filters * 2, (1, 1), padding='same', kernel_initializer=he_normal(),
-                   kernel_regularizer=l2(self.l2_weight))(x)
+                   kernel_regularizer=l2(self.l2_weight), use_bias=False)(x)
         x = BatchNormalization()(x)
 
         x = add([x_skip, x])
@@ -67,10 +70,13 @@ class ResNeXtModel:
         nb_filters = self.cardinality * self.width
         for stage, nb_block in enumerate(self.structure):
             for block_id in range(nb_block):
-                if stage > 0 and block_id == 0:
-                    x = self.bottleneck_block(x, nb_filters, self.cardinality, 2)
+                if block_id == 0:
+                    if stage > 0:
+                        x = self.bottleneck_block(x, nb_filters, 2, pass_filter=True)
+                    else:
+                        x = self.bottleneck_block(x, nb_filters, 1, pass_filter=True)
                 else:
-                    x = self.bottleneck_block(x, nb_filters, self.cardinality, 1)
+                    x = self.bottleneck_block(x, nb_filters, 1)
 
             nb_filters *= 2
 
@@ -79,24 +85,21 @@ class ResNeXtModel:
         return model
 
     def fit(self, x, y, x_test, y_test, batch_size, epochs):
-        self.model.fit(x=x, y=y, validation_data=(x_test, y_test), batch_size=batch_size, epochs=epochs)
+        self.model.fit(x=x, y=y, validation_data=(x_test, y_test), batch_size=batch_size, epochs=epochs, verbose=2)
 
-    def test(self, x, y):
-        y_pred = self.model.predict(x)
-        y_pred = np.argmax(y_pred, axis=1)
-        accuracy = accuracy_score(y, y_pred)
-        print(accuracy)
-
-    def grouped_block(self, inputs, group_size, cardinality, strides):
+    def grouped_block(self, inputs, group_size, strides):
         group_list = []
 
-        for i in range(cardinality):
+        for i in range(self.cardinality):
             x = Lambda(lambda _inputs: _inputs[:, :, :, i * group_size:(i + 1) * group_size])(inputs)
             x = Conv2D(group_size, (3, 3), padding='same', kernel_initializer=he_normal(),
-                       kernel_regularizer=l2(self.l2_weight), strides=(strides, strides))(x)
+                       kernel_regularizer=l2(self.l2_weight), strides=(strides, strides), use_bias=False)(x)
             group_list.append(x)
 
         x = concatenate(group_list)
         x = BatchNormalization()(x)
         x = Activation(relu)(x)
         return x
+
+    def get_model(self):
+        return self.model
